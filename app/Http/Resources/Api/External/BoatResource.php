5afdccd59be075d53e9a17edafc5475a7bb9696e
Brandy <26582143+AlteriaX@users.noreply.github.com>
2026-09-25 11:24:40 +0800

    Fix conflicts

--- app/Http/Resources/Api/External/BoatResource.php
+++ app/Http/Resources/Api/External/BoatResource.php
@@ -6,26 +6,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BoatResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'company_id' => $this->company_id,
             'number' => $this->number,
-            'license' => $this->license,
-            'license_expiry_date' => $this->license_expiry_date,
-            'capacity' => $this->capacity,
-            'company' => [
-                'id' => $this->company->id,
-                'name' => $this->company->name,
-            ],
-            'boatman' => BoatmanResource::collection($this->whenLoaded('boatman')),
         ];
     }
 }
--- app/Http/Resources/Api/External/BoatmanResource.php
+++ app/Http/Resources/Api/External/BoatmanResource.php
@@ -6,29 +6,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BoatmanResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
             'boat_id' => $this->boat_id,
+            'company_id' => $this->company_id,
             'name' => $this->name,
             'ic_no' => $this->ic_no,
-            'mate_card' => $this->mate_card,
-            'seaman_card_no' => $this->seaman_card_no,
             'type' => $this->type,
-            'type_label' => $this->typeLabel(),
-            'company' => $this->whenLoaded('company', function () {
-                return [
-                    'id' => $this->company->id,
-                    'name' => $this->company->name,
-                ];
-            }),
         ];
     }
 }
